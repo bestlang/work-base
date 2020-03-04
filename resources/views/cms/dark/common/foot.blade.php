7d7cabb5659886d2cbfcde853e887ee1c848dcd5
marco <luzhang987@163.com>
2020-03-04 10:47:14 +0800

    #

--- resources/views/cms/dark/common/foot.blade.php
+++ resources/views/cms/dark/common/foot.blade.php
@@ -1,7 +1,7 @@
 <div class="l-foot">
     <div class="l-row">
         <div class="l-special-remark col-xl-4 col-md-6 col-xs-12">
-            <div class="l-remark-title"><b>{{ config('app.name') }}</b>content management system</div>
+            <div class="l-remark-title"><b>{{ config('app.name') }}</b> content management system</div>
             <div class="l-remark-content">a laravel based system. <br />a good php cms!</div>
         </div>
         <div class="l-group-link col-xl-4 col-md-6 col-xs-12"></div>
