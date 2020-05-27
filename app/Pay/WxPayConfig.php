30cca92ccb7dc1e0e87f86137e3218f69c4f2071
marco <luzhang987@163.com>
2020-05-27 10:16:30 +0800

    #native支付方法1,2

--- app/Pay/WxPayConfig.php
+++ app/Pay/WxPayConfig.php
@@ -38,7 +38,7 @@ class WxPayConfig extends WxPayConfigInterface
      **/
     public function GetNotifyUrl()
     {
-        return "https://www.laracms.com/notify/wechat/async";
+        return "https://www.laracms.com/notify/wechat/native";
     }
     public function GetSignType()
     {
