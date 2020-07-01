f48f0e61e982b3cb077be2ee2840b0cdeea235a0
marco <luzhang987@163.com>
2020-07-01 17:02:54 +0800

    #持续完善

--- resources/admin/config/prod.env.js
+++ resources/admin/config/prod.env.js
@@ -1,6 +1,7 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  SITE_URL: '"http://127.0.0.1:8000"',
+  // SITE_URL: '"http://127.0.0.1:8000"',
+  SITE_URL: '"http://www.laracms.com"',
   ADMIN_URL: '"/admin"'
 }
