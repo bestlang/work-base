c191bb8188ca0f40dba707d5aff41913478c576c
luzhang987@163.com <620510love>
2020-09-21 10:13:48 +0800

    #recompile admin assets

--- resources/admin/config/prod.env.js
+++ resources/admin/config/prod.env.js
@@ -4,6 +4,6 @@ module.exports = {
   APP_SHORT_NAME: 'SN',
   NODE_ENV: '"production"',
   //SITE_URL: '"http://127.0.0.1:8000"',
-  SITE_URL: '"http://sniper.dev"',
+  SITE_URL: '"http://39.99.224.136"',
   ADMIN_URL: '"/admin"'
 }
