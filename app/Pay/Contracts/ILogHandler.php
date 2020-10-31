176ff6cf792d680f19165fc16c4a848b98984191
marco <luzhang987@163.com>
2020-10-31 20:51:25 +0800

    #

--- app/Pay/Contracts/ILogHandler.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace App\Pay\Contracts;
-
-interface ILogHandler
-{
-    public function write($msg);
-
-}
--- app/Pay/Exceptions/WxPayException.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace App\Pay\Exceptions;
-
-class WxPayException extends \Exception {
-    public function errorMessage()
-    {
-        return $this->getMessage();
-    }
-}
