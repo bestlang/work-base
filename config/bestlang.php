75a9dd8234372e105dc95383b4ff941c7eb0c34f
luzhang987@163.com <620510love>
2020-11-06 14:56:54 +0800

    #

--- config/bestlang.php
+++ config/bestlang.php
@@ -1,5 +1,5 @@
 <?php
 return [
-    'defaultApplicationPath' => env('DEFAULT_APPLICATION_PATH', '/sniper'),
-    'authViewNamespace' => 'sniper'
+    'defaultApplicationPath' => env('DEFAULT_APPLICATION_PATH', '/base'),
+    'authViewNamespace' => env('AUTH_VIEW_NAMESPACE', 'base')
 ];
