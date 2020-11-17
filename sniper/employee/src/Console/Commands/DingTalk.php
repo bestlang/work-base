09a50526feb30efb6b6f1a6c02e85444da6cb149
luzhang987@163.com <620510love>
2020-11-17 10:56:25 +0800

    #

--- sniper/employee/src/Console/Commands/DingTalk.php
+++ sniper/employee/src/Console/Commands/DingTalk.php
@@ -163,12 +163,6 @@ class DingTalk extends Command
                             //'avatar' => null,
                             //扩展信息
                         ]);
-                    }else if($user && !$user->emplyee){
-                        $user->employee()->create([
-                            'real_name' => $dingUser->name,
-                            'department_id' => $dingUser->department,//使用dingTalk自带的department编号插入
-                            'avatar' => $dingUser->avatar
-                        ]);
                     }else{
                         $user->employee->real_name = $dingUser->name;
                         $user->employee->department_id = $dingUser->department;
