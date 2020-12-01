dcf06f5422f97954d8c023aab2787100d6468228
marco <luzhang987@163.com>
2020-12-01 23:00:26 +0800

    #

--- bestlang/laracms/src/Http/Controllers/UCenterController.php
+++ bestlang/laracms/src/Http/Controllers/UCenterController.php
@@ -3,19 +3,25 @@
 namespace BestLang\LaraCms\Http\Controllers;
 
 use Illuminate\Http\Request;
+use HashConfig;
 
 class UCenterController extends Controller
 {
+    public $theme;
+    public function __construct()
+    {
+        $this->theme = HashConfig::get('site', 'theme');
+    }
     public function index(Request $request)
     {
         if(!auth()->user()){
             return redirect('/login');
         }
-        return render('ucenter.index');
+        return view("laracms::{$this->theme}.ucenter.index");
     }
 
     public function passwordModifyForm(Request $request)
     {
-        return render('ucenter.passwordModifyForm');
+        return view("laracms::{$this->theme}.ucenter.passwordModifyForm");
     }
 }
