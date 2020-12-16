553c41af8fd9dfe7d07f68fa883da05c8020a897
luzhang987@163.com <620510love>
2020-12-16 19:06:37 +0800

    #notice table

---
+++ sniper/employee/database/migrations/2020_12_16_174232_create_sniper_employee_notices_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSniperEmployeeNoticesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sniper_employee_notices', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('user_id');
+            $table->string('title')->comment('公告主题');
+            $table->dateTime('published_at')->comment('发布时间');
+            $table->text('note')->comment('备注')->nullable();
+            $table->text('content')->comment('公告正文');
+            $table->text('attachments')->comment('附件列表');
+            $table->tinyInteger('sent')->comment('发布标志0未发布1已发布');
+            $table->nullableTimestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sniper_employee_notices');
+    }
+}
