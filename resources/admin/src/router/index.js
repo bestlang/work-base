aee3c6eae7dcb2d879f9bc75b2fca55fb9396733
luzhang987@163.com <620510love>
2020-12-18 19:59:50 +0800

    #

--- resources/admin/src/router/index.js
+++ resources/admin/src/router/index.js
@@ -12,7 +12,7 @@ let pkg = require('../../package')
 
 let children = [];
 let allowed = pkg.allowModules;
-let allModules = {sniper, panel, cms, base, privileges}
+let allModules = {sniper, cms, base, privileges}
 for(let key in allModules){
     if(allowed.indexOf(key) !== -1){
         children = children.concat(allModules[key])
