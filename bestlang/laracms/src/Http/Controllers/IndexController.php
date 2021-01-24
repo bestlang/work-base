e6a23af2429dc1fc90d055e5a8f15d56ae4be7bc
marco <luzhang987@163.com>
2021-01-24 23:18:27 +0800

    #

--- bestlang/laracms/src/Http/Controllers/IndexController.php
+++ bestlang/laracms/src/Http/Controllers/IndexController.php
@@ -11,19 +11,4 @@ class IndexController extends Controller
     {
         return render('index.index');
     }
-
-    public function cms()
-    {
-        return render('index.cms');
-    }
-
-    public function about()
-    {
-        return render('index.about');
-    }
-
-    public function contact()
-    {
-        return render('index.contact');
-    }
 }
